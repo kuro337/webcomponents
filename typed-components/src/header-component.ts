d3e1e5432f7d0da96fb0e7eb216c54680e638005
Chinmay <cb.kalz@gmail.com>
2023-08-06 01:17:46 -0400

    google header material

--- typed-components/src/header-component.ts
+++ typed-components/src/header-component.ts
@@ -1,23 +1,42 @@
 import { LitElement, html, css } from "lit";
 import { customElement, property } from "lit/decorators.js";
+import "@material/mwc-top-app-bar";
+import "@material/mwc-icon-button";
 
 @customElement("header-component")
 export class HeaderComponent extends LitElement {
-  @property()
-  someProp = "Random Value";
+  @property({ type: String })
+  headerText = "Header";
 
   static styles = css`
     :host {
       display: block;
       font-family: sans-serif;
-      text-align: center;
+    }
+
+    mwc-top-app-bar {
+      --mdc-theme-primary: #862e9c; /* Adjust primary color if needed */
+      --mdc-theme-on-primary: #f8f9fa; /* Adjust text/icon color if needed */
     }
   `;
 
   render() {
     return html`
-      <p>Welcome to the Lit tutorial!</p>
-      <p>This is from the ${this.someProp} prop.</p>
+      <mwc-top-app-bar>
+        <mwc-icon-button slot="navigationIcon" icon="menu"></mwc-icon-button>
+        <div slot="title">${this.headerText}</div>
+        <mwc-icon-button slot="actionItems" icon="bookmark"></mwc-icon-button>
+      </mwc-top-app-bar>
     `;
   }
 }
+
+/*
+
+pnpm install @material/mwc-top-app-bar @material/mwc-icon-button
+
+Include this for icons to show up 
+
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+
+*/
